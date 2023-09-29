9b793d26a68f3c11fd8ffedd6152d569e47eaa80
Rahatk1 <rahatkhandokar5@gmail.com>
2023-09-28 22:58:39 -0400

    added script tag to HTML and finshed fillAll

--- fillAll.js
+++ fillAll.js
@@ -1,1 +1,6 @@
 document.getElementById("colorAll").addEventListener("click", colorAll); // added an event handler that gets tje elemnt from the id "color all" Then it will execute the function 
+function changeCellColor(event) { //created a new fucntion that changes the color of each cell
+    if (event.target.tagName === 'TD') { // if statment that checks that of the elemnt that was cliocked has a TD tag whocjh is tablescell
+        event.target.style.backgroundColor = document.getElementById("colorPicker").value; //grabs value of color and chnages the cell colors
+    }
+}
