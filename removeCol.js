8feb6010c8bfdf283b2c43a00d9e791eec731906
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2023-10-01 15:06:57 -0400

    added remove column function and eventListener

---
+++ removeCol.js
@@ -0,0 +1,11 @@
+// retrieve element named removeColumn after a click event
+document.getElementById("removeColumn").addEventListener("click", removeColumn);
+
+// removing column from div
+function removeColumn() {
+    if (table.rows.length > 0 && table.rows[0].cells.length > 0) {
+        for (let row of table.rows) {
+            row.deleteCell(-1);
+        }
+    }
+}
